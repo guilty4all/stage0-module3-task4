78e3bfa890dcb1c65e960d22dd05d46fb8914927
guilt <gulilty4all@gmail.com>
2022-08-16 16:53:13 +0600

    third solution

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -4,6 +4,6 @@ public class FormulaCreator {
     public static void main(String[] args) {
         float a = 1;
         float b = 2;
-        System.out.println((9*Math.pow(a,2)-5*b+2+a-7)*((a+b-4*a*b)/2));
+        double formula = (9 * a * a - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2);
     }
 }
--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,13 +2,13 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
-        int c = 'c';
-        int h = 'h';
-        int a = 'a';
-        int r = 'r';
-        System.out.println(c);
-        System.out.println(h);
-        System.out.println(a);
-        System.out.println(r);
+        int charC ='c';
+        int charH = 'h';
+        int charA = 'a';
+        int charR = 'r';
+        System.out.println(charC);
+        System.out.println(charH);
+        System.out.println(charA);
+        System.out.println(charR);
     }
 }
